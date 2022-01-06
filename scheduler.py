32f7b3de363fd57fc0d889645db59467c72c974b
Origami <q@timvel.com>
2022-01-06 14:59:56 +0800

    add timezone

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.redis import RedisJobStore
+from pytz import utc
 from .ec2Handler import stop_ec2
 from .logger import logger
 jobstores = RedisJobStore(
@@ -7,9 +8,9 @@ jobstores = RedisJobStore(
     port=6379,
     db=0
 )
-sched = BackgroundScheduler(jobstores=jobstores)
+sched = BackgroundScheduler(jobstores=jobstores, timezone=utc)
 
 
 def schedule_to_shut_down_ec2(instance_id):
-    success,resp = stop_ec2(instance_id)
+    success, resp = stop_ec2(instance_id)
     logger.info(f'[SCHEDULE] [Stop ec2] {success},{resp}')
