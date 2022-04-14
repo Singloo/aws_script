a22d8f34ad11c72660b42f596a772549a80dead8
Origami <q@timvel.com>
2022-04-14 22:06:28 +0800

    add validator type

--- src/types/type.py
+++ src/types/type.py
@@ -1,5 +1,15 @@
-from typing import TypedDict
+from typing import TypedDict, Callable
 
 
 class CachedData(TypedDict):
     instance_id: str
+
+
+ValidatorFunc = Callable[[str], bool]
+
+
+class Validator(TypedDict):
+    prompt: str
+    attribute_name: str
+    validator: ValidatorFunc
+    value: str | None
