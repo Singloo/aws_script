32cf4ac458d171b5560441ea52d777e6e026e75c
Origami <q@timvel.com>
2022-04-23 19:33:16 +0800

    validator

--- src/messageHandlers/InputValidator.py
+++ src/messageHandlers/InputValidator.py
@@ -1,5 +1,74 @@
-from src.types import ValidatorFunc, Validator
+from src.types import ValidatorFunc
 import time
+from src.types.type import CachedData
+from src.utils.util import list_every
+
+
+class SessionExpiredException(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__('Session expired, try again.', *args)
+
+
+class SessionFinished(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__('Session finished', *args)
+
+
+class ValidatorInvalidInput(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__('Invalid input', *args)
+
+
+class ValidatorExceedMaximumTimes(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__('Exceeded maxium times', *args)
+
+
+class Validator():
+    # prompt msg
+    _prompt: str
+    # prompt msg when input is invalid
+    _invalid_prompt: str
+    # attribute name
+    _attribute_name: str
+    # test if input is valid
+    _validator: ValidatorFunc
+    # input value from user
+    _value: str | None
+    # retried times
+    _times: int
+    # should encrypt value
+    _encrypt: bool
+
+    def __init__(self, prompt: str, invalid_prompt: str, attribute_name: str, validator: ValidatorFunc, encrypt: bool) -> None:
+        self._prompt = prompt
+        self._invalid_prompt = invalid_prompt
+        self._attribute_name = attribute_name
+        self._validator = validator
+        self._encrypt = encrypt
+
+        self._value = None
+        self._times = 0
+        self.MAX_TIMES = 3
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def is_max_times_exceeded(self):
+        return self._times > self.MAX_TIMES
+
+    @value.setter
+    def value(self, newValue: str):
+        self._times += 1
+        if self.is_max_times_exceeded:
+            raise ValidatorExceedMaximumTimes()
+        if not isinstance(newValue, str):
+            raise ValidatorInvalidInput()
+        if not self._validator(newValue):
+            raise ValidatorInvalidInput()
+        self._value = newValue
 
 
 class InputValidator():
@@ -19,5 +88,18 @@ class InputValidator():
     def is_expired(self):
         return time.time() > self.end_time
 
+    @property
+    def is_finished(self):
+        if self.current_idx < len(self._validators) - 1:
+            return False
 
-    
+        def _every_validator_got_answer(validator: Validator):
+            return validator.value != None
+
+        return list_every(self._validators, _every_validator_got_answer)
+
+    def get_one(self):
+        if self.is_expired:
+            raise SessionExpiredException()
+        if self.is_finished:
+            raise SessionFinished()
--- src/utils/util.py
+++ src/utils/util.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import Any, TypeVar, Callable
 from src.logger import logger
 TIME_OUT_MSG = "Sorry, operation didn't finish on time, task is still running, please check it out later"
 
@@ -13,3 +14,17 @@ async def async_race(*fs):
     tasks = [loop.create_task(f) for f in fs]
     done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
     return [o.result() for o in done]
+
+T = TypeVar('T')
+
+
+def list_every(list: list[T], handler: Callable[[T], bool]) -> bool:
+    '''
+        same as js array.every
+    '''
+    res = True
+    for item in list:
+        if handler(item) == False:
+            res = False
+            break
+    return res
