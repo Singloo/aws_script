861bd99316ae2289931314f13a27e9cbf6de977a
Origami <q@timvel.com>
2022-05-10 09:38:14 +0800

    add async handler

--- src/messageHandlers/__init__.py
+++ src/messageHandlers/__init__.py
@@ -9,14 +9,27 @@ class NoSuchHandler(Exception):
 class BaseMessageHandler():
     def __init__(self,params:Any = {}) -> None:
         self.params = params
+
     def __call__(self, cmds: list[str]):
         first = cmds[0] 
         res = getattr(self, first, None)
         if res is None:
-            self.fallback()
-        # is_child_instance = res.__base__ in self.__class__.__bases__
-        # if is_child_instance:
-        #     return res(self.params)(cmds[1:])
+            return self.fallback()
         return res(cmds[1:])
+
     def fallback(self):
+        raise NoSuchHandler
+
+class AsyncBaseMessageHandler():
+    def __init__(self,params:Any = {}) -> None:
+        self.params = params
+
+    async def __call__(self, cmds: list[str]):
+        first = cmds[0] 
+        res = getattr(self, first, None)
+        if res is None:
+            return await self.fallback()
+        return await res(cmds[1:])
+        
+    async def fallback(self):
         raise NoSuchHandler
