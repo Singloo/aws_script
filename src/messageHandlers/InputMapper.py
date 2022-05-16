c187d662de7408e137404196f8651bc6e901be66
Origami <q@timvel.com>
2022-05-16 09:50:14 +0800

    use async handler,optimize way of initialization

--- src/messageHandlers/InputMapper.py
+++ src/messageHandlers/InputMapper.py
@@ -1,11 +1,17 @@
 from typing import Any
-from . import BaseMessageHandler
+from . import AsyncBaseMessageHandler
 from .awsHandler import AwsHandler
 from .ec2Handler import Ec2Handler
+
 def destruct_msg(msg: str) -> list[str]:
     return msg.lower().strip().split(' ')
 
 
-class InputMapperEntry(BaseMessageHandler):
-    aws = AwsHandler
-    ec2 = Ec2Handler
+class InputMapperEntry(AsyncBaseMessageHandler):
+    @property
+    def aws(self):
+        return AwsHandler(self.params)
+
+    @property
+    def ec2(self):
+        return Ec2Handler(self.params)
--- src/messageHandlers/awsHandler.py
+++ src/messageHandlers/awsHandler.py
@@ -1,24 +1,64 @@
-from . import BaseMessageHandler
+from . import AsyncBaseMessageHandler
+from .InputValidator import ValidatorManager, Validator
 
 
-class AwsBind(BaseMessageHandler):
-    def __call__(self, cmds: list[str]):
+class AwsBind(AsyncBaseMessageHandler):
+    async def __call__(self, cmds: list[str]):
         print('bind')
 
 
-class AwsList(BaseMessageHandler):
-    def __call__(self, cmds: list[str]):
+class AwsList(AsyncBaseMessageHandler):
+    async def __call__(self, cmds: list[str]):
         print('list', cmds)
 
 
-class AwsRm(BaseMessageHandler):
-    def __call__(self, cmds: list[str]):
+class AwsRm(AsyncBaseMessageHandler):
+    async def __call__(self, cmds: list[str]):
         print('rm', cmds)
 
 
-class AwsHandler(BaseMessageHandler):
-    bind = AwsBind
+class AwsHandler(AsyncBaseMessageHandler):
+    @property
+    def bind(self):
+        return AwsBind(self.params)
 
-    list = AwsList
+    @property
+    def list(self):
+        return AwsList(self.params)
 
-    rm = AwsRm
+    @property
+    def rm(self):
+        return AwsRm(self.params)
+
+
+regions = ["ap", "us", "ca", "eu", "me", "af", "sa", "cn"]
+
+orientations = ['north', 'west', 'east', 'south',
+                'southeast', 'southwest', 'northeast', 'northwest']
+
+region_nums = ['1', '2', '3', '4']
+
+region_regex_str = f"^({'|'.join(regions)})-({'|'.join(orientations)})-({'|'.join(region_nums)})$"
+
+AWS_VALIDATORS: list[Validator] = [
+    Validator(
+        prompt='aws key id',
+        invalid_prompt='aws key id is wrong',
+        attribute_name='aws_key_id',
+        validator=partial(re_strict_match, pattern='^[A-Z0-9]{20}$'),
+        encrypt=True
+    ),
+    Validator(
+        prompt='aws secret access key',
+        invalid_prompt='aws secret access key is wrong',
+        attribute_name='aws_secret_access_key',
+        validator=partial(re_test, pattern='^[a-zA-Z0-9]{40}$'),
+        encrypt=True
+    ),
+    Validator(
+        prompt='region name',
+        invalid_prompt='region name is invalid',
+        attribute_name='region_name',
+        validator=partial(re_strict_match, pattern=region_regex_str)
+    ),
+]
