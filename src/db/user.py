1e0bc27ba733f454853341ba6402e9eb8a309328
Origami <q@timvel.com>
2022-05-30 09:43:17 +0800

    user repo

--- src/db/user.py
+++ src/db/user.py
@@ -1,6 +1,7 @@
 from .mongo import Mongo
 from src.types import User
 from bson.objectid import ObjectId
+from datetime import datetime
 
 
 class UserRepo(Mongo):
@@ -9,6 +10,17 @@ class UserRepo(Mongo):
         self.col = self.get_collection('user')
 
     def insert(self, doc: User) -> ObjectId:
-        res = self.col.insert_one(doc)
+        res = self.col.insert_one(
+            {**self.add_created_updated_at(doc), 'activated_at': datetime.now()})
         return res.inserted_id
 
+    def find_by_wechat_id(self, wechat_id: str) -> User:
+        doc = {
+            'wechat_id': wechat_id
+        }
+        res = self.col.find_one(doc)
+        if res is None:
+            self.insert(doc)
+        else:
+            self.col.update_one(doc, {'activated_at': datetime.now()})
+        return res
